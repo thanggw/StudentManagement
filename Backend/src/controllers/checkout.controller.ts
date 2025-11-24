4def4ec7b2a937ae38eb50357b228d655683819e
thanggw <thangvuvan0611@gmail.com>
2025-11-24 14:12:38 +0700

    update checkout api

--- Backend/src/controllers/checkout.controller.ts
+++ Backend/src/controllers/checkout.controller.ts
@@ -1,17 +1,23 @@
-import {post, requestBody} from '@loopback/rest';
+import {HttpErrors, post, requestBody} from '@loopback/rest';
 import {inject} from '@loopback/core';
 import {CheckoutService} from '../services/checkout.service';
-import {authenticate} from '@loopback/authentication';
+import {authenticate, AuthenticationBindings} from '@loopback/authentication';
+import {MyUserProfile} from '../dto/MyUserProfile';
 
 export class CheckoutController {
   constructor(
     @inject('services.CheckoutService')
     private checkoutService: CheckoutService,
+    @inject(AuthenticationBindings.CURRENT_USER)
+    private currentUser: MyUserProfile,
   ) {}
   @authenticate('jwt')
   @post('/checkout')
-  async checkout(@requestBody() body: {studentId: string}) {
-    const studentId = body.studentId;
+  async checkout() {
+    const studentId = this.currentUser.id;
+    if (!studentId) {
+      throw new HttpErrors.Forbidden('Không thể xác thực người dùng');
+    }
     return this.checkoutService.checkout(studentId);
   }
 }
