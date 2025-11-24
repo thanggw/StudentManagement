da31575ac5f66729627ab282cc711187cc60088a
thanggw <thangvuvan0611@gmail.com>
2025-11-24 17:06:11 +0700

    update

--- Backend/src/controllers/checkout.controller.ts
+++ Backend/src/controllers/checkout.controller.ts
@@ -2,14 +2,14 @@ import {HttpErrors, post, requestBody} from '@loopback/rest';
 import {inject} from '@loopback/core';
 import {CheckoutService} from '../services/checkout.service';
 import {authenticate, AuthenticationBindings} from '@loopback/authentication';
-import {MyUserProfile} from '../dto/MyUserProfile';
+import {MyUserTokenProfile} from '../dto/user-profile.types';
 
 export class CheckoutController {
   constructor(
     @inject('services.CheckoutService')
     private checkoutService: CheckoutService,
     @inject(AuthenticationBindings.CURRENT_USER)
-    private currentUser: MyUserProfile,
+    private currentUser: MyUserTokenProfile,
   ) {}
   @authenticate('jwt')
   @post('/checkout')
--- Backend/src/dto/MyUserProfile.ts
+++
@@ -1,8 +0,0 @@
-import {securityId, UserProfile as LBUserProfile} from '@loopback/security';
-
-export interface MyUserProfile extends LBUserProfile {
-  [securityId]: string;
-  id: string;
-  name: string;
-  email: string;
-}
